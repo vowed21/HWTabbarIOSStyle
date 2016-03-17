9ee4c54b5ba55fb7e8384b3dd968206da98109a0
HyunWoo Kim <hyunwoo-21@hanmail.net>
2016-03-17 14:52:22 +0900

    1.1 jcenter 에 올라간 커밋.

---
+++ app/src/main/java/com/kokohapps/hwtabbar_ios_style_master/MainActivity.java
@@ -0,0 +1,106 @@
+package com.kokohapps.hwtabbar_ios_style_master;
+
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.kokohapps.hwtabbar_ios_style.HWTabbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    HWTabbar tabbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(new Adapter(getSupportFragmentManager()));
+
+        tabbar = (HWTabbar) findViewById(R.id.tabbarController);
+
+        String[] titleArr = {"탭", "탭22", "탭3333", "탭4444"};
+        Integer[] imgSrcArr = {R.drawable.off, R.drawable.off, R.drawable.off, R.drawable.off};
+        Integer[] selectedImgSrcArr = {R.drawable.on, R.drawable.on, R.drawable.on, R.drawable.on};
+
+
+
+        try {
+            tabbar.initTabbar(viewPager, titleArr, imgSrcArr, selectedImgSrcArr, Color.BLUE);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        tabbar.setBadge(2, "Wow~!");
+        tabbar.setBadge(0, "1");
+
+
+    }
+
+
+
+
+
+
+
+
+    private class Adapter extends FragmentPagerAdapter {
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return MyFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+    }
+
+
+    public static class MyFragment extends Fragment {
+
+        int position;
+
+        public static MyFragment newInstance(int position) {
+
+            MyFragment fragment = new MyFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt("position", position);
+            fragment.setArguments(bundle);
+            fragment.position = position;
+            return fragment;
+
+        }
+
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+            TextView tv = new TextView(getActivity());
+            tv.setText("PAGE "+position);
+            tv.setGravity(Gravity.CENTER);
+            tv.setTextSize(30);
+            return tv;
+        }
+    }
+}
